4db601a29a87ed0748f56c58d0c18dbfd169c207
Staxeedo <33715383+Staxeedo@users.noreply.github.com>
2017-11-18 22:29:29 +0100

    pettern

--- src/cz/uhk/pgrf1/c03/madr/uloha2/render/SeedFillRenderer.java
+++ src/cz/uhk/pgrf1/c03/madr/uloha2/render/SeedFillRenderer.java
@@ -11,15 +11,32 @@ public class SeedFillRenderer extends Renderer {
 	}
 
 	public void draw(Point p, int color) {
+		//vzor
+		int[][] vzor = {
+				{0xFF0000,0xFF0000,0xFF0000},
+				{0xFF0000,0xFF0000,0xFF0000},
+				{0xFF0000,0xFF0000,0xFF0000},
+				{0xFF0000,0xFF0000,0xFF0000},
+				{0xFFFFFF,0xFFFFFF,0xFFFFFF},
+				{0xFFFFFF,0xFFFFFF,0xFFFFFF},
+				{0xFFFFFF,0xFFFFFF,0xFFFFFF},
+				{0xFFFFFF,0xFFFFFF,0xFFFFFF}
+				
+		};
 		int x = (int)p.getX();
 		int y= (int)p.getY();
+		int i = x % 8;
+		int	j = y % 3;
+		int pcolor=vzor[i][j];
+		
 		// point celociselna pozice v img
 		int bgColor = img.getRGB(x, y);
 		
 		// if jsme uvnitr?
 		if (bgColor == color) {
 
-			img.setRGB(x, y, 0xFFFFCA);
+			//img.setRGB(x, y, 0xFFFFCA);
+			img.setRGB(x, y, pcolor);
 			draw(new Point(x + 1, y), color);
 			draw(new Point(x, y + 1), color);
 			draw(new Point(x - 1, y), color);
